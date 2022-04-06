5aaf7b19e3e73ec2b560efdb3c5f69c5a05ea9e8
yukitteru <55682148+yukitteru@users.noreply.github.com>
2022-04-06 15:13:14 +0500

    Update Calculator.java

--- src/main/java/net/yukitteru/calculator/calculator/Calculator.java
+++ src/main/java/net/yukitteru/calculator/calculator/Calculator.java
@@ -12,18 +12,19 @@ public class Calculator {
     private final ReversePolishNotation reversePolishNotation;
 
 
-    public Object calculate(String infixExpression) throws IllegalExpressionException {
+    public Object calculate(String infixExpression) throws IllegalExpressionException, IncorrectResultValueException {
         String postfix = infixToPostfixConverter.convert(infixExpression);
         Integer eval = reversePolishNotation.eval(postfix.split(" "));
-
         try {
             if (postfix.chars().anyMatch(Character::isDigit)) {
-                return reversePolishNotation.eval(postfix.split(" "));
+                return eval;
             } else {
                 return NumerationConverter.arabicToRoman(eval);
             }
         } catch (Exception e) {
-            throw new IllegalExpressionException("The application only works with integers from 0 to 10");
+            if (eval < 1) {
+                throw new IncorrectResultValueException("Roman numerals cannot be less than 1");
+            } else throw new IllegalExpressionException("The based expression is not valid. Try again");
         }
 
     }
