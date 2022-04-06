c82ffe3b3453fc7d3d3a6eb03e830412d6b0c191
yukitteru <55682148+yukitteru@users.noreply.github.com>
2022-04-06 17:31:17 +0500

    Fixed incorrect tests. Added Exceptions Tests

--- src/test/java/net/yukitteru/calculator/ApplicationTest.java
+++ src/test/java/net/yukitteru/calculator/ApplicationTest.java
@@ -2,10 +2,13 @@ package net.yukitteru.calculator;
 
 import net.yukitteru.calculator.calculator.Calculator;
 import net.yukitteru.calculator.exception.IllegalExpressionException;
+import net.yukitteru.calculator.exception.IncorrectResultValueException;
 import net.yukitteru.calculator.util.ExpressionParser;
 import net.yukitteru.calculator.util.InfixToPostfixConverter;
 import net.yukitteru.calculator.util.ReversePolishNotation;
 import org.junit.Test;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,96 +16,154 @@ public class ApplicationTest {
     @Test
     public void itShouldEvaluateTheGivenExpressionWithArabicNumbersAdditionalOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "5 + 8";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals(13, result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals(13, result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithArabicNumbersSubtractionOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "10 - 6";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals(4, result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals(4, result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithArabicNumbersMultiplicationOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "9 * 7";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals(63, result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals(63, result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithArabicNumbersDivisionOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "8 / 4";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals(2, result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals(2, result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithRomeNumbersPlusOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "IV + IX";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals("XIII", result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals("XIII", result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithRomeNumbersSubtractionOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "IX - III";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals("VI", result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals("VI", result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithRomeNumbersMultiplicationOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "III * VII";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals("XXI", result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals("XXI", result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 
     @Test
     public void itShouldEvaluateTheGivenExpressionWithRomeNumbersDivisionOperationAndReturnTheResult() throws IllegalExpressionException, IncorrectResultValueException {
         String infixExpression = "X / II";
-        var expressionParser  = new ExpressionParser();
-        var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
-        var reversePolishNotation = new ReversePolishNotation();
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
 
-        var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
-        var result = calculator.calculate(infixExpression);
-        assertEquals("V", result);
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            assertEquals("V", result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
+    }
+
+    @Test(expected = IncorrectResultValueException.class)
+    public void itShouldEvaluateTheGivenExpressionWithRomeNumbersWithNegativeNumbersAndReturnTheResult() throws IncorrectResultValueException, IllegalExpressionException {
+        String infixExpression = "V - VI";
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
+
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            System.out.println("Your result is: " + result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void itShouldEvaluateTheGivenExpressionWithRomeNumbersWithANumberGreaterThenTensAndReturnTheResult() throws IncorrectResultValueException, IllegalExpressionException {
+        String infixExpression = "C + I";
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
+
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            System.out.println("Your result is: " + result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
+    }
+
+    @Test(expected = IllegalExpressionException.class)
+    public void itShouldEvaluateTheGivenExpressionWithRomeNumbersWithArabicNumbersAndReturnTheResult() throws IncorrectResultValueException, IllegalExpressionException {
+        String infixExpression = "V + 7";
+        if (ExpressionParser.isValidExpr(infixExpression)) {
+            var expressionParser = new ExpressionParser();
+            var infixToPostfixConverter = new InfixToPostfixConverter(expressionParser);
+            var reversePolishNotation = new ReversePolishNotation();
+
+            var calculator = new Calculator(infixToPostfixConverter, reversePolishNotation);
+            Object result = calculator.calculate(infixExpression);
+            System.out.println("Your result is: " + result);
+        } else throw new IllegalExpressionException("The based expression is not valid. Try again");
     }
 }
